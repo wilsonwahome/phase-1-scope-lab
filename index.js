82559a5b66b268e0c129dce4758d4a2fa0dae5ee
wahome <wahomewgitonga@gmail.com>
2024-03-24 01:27:50 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "customer";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new least favorite";
+}
